3479a90c0faa8971f409f93508f1fa600bf7db73
MikeKowalsky <mkowalewski83@gmail.com>
2019-01-20 18:52:47 +0100

    added PostPreview component and added styles to main/posts page

--- pages/index.vue
+++ pages/index.vue
@@ -1,68 +1,57 @@
 <template>
-  <section class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        storyblok-blog
-      </h1>
-      <h2 class="subtitle">
-        Learning
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >GitHub</a>
-      </div>
-    </div>
+  <section id="posts">
+    <PostPreview 
+      v-for="post in posts"
+      :key="post.id"
+      :title="post.title"
+      :excerpt="post.previewText"
+      :thumbnailImage="post.thumbnailUrl"
+      :id="post.id"
+      />
   </section>
 </template>
 
 <script>
-import Logo from "~/components/Logo.vue";
-
+import PostPreview from "@/components/Blog/PostPreview";
 export default {
   components: {
-    Logo
+    PostPreview
+  },
+  data() {
+    return {
+      posts: [
+        {
+          title: "Test post",
+          previewText: "This will be cool, wait for it",
+          thumbnailUrl:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIY5J78PoCCQcZzDFT4BQL5TWxnqaCXlvSANz2x_fKh-Jh3HkvVg",
+          id: "test-post "
+        },
+        {
+          title: "Test second",
+          previewText: "This will be cool, wait for it",
+          thumbnailUrl:
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIY5J78PoCCQcZzDFT4BQL5TWxnqaCXlvSANz2x_fKh-Jh3HkvVg",
+          id: "test-second"
+        }
+      ]
+    };
   }
 };
 </script>
 
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
+<style scoped>
+#posts {
+  padding-top: 2rem;
   display: flex;
   justify-content: center;
   align-items: center;
-  text-align: center;
+  flex-direction: column;
 }
 
-.title {
-  font-family: "Quicksand", "Source Sans Pro", -apple-system, BlinkMacSystemFont,
-    "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
+@media (min-width: 35rem) {
+  #posts {
+    flex-direction: row;
+  }
 }
 </style>
